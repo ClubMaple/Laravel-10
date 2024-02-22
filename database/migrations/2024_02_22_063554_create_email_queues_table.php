f91c48328ab2a77a505fa21e458fb1d9f5122b04
FeFerxo08 <fernandopalacios751@gmail.com>
2024-02-21 21:43:20 -0600

    Crear la migración de la tabla

--- database/migrations/2024_02_22_063554_create_email_queues_table.php
+++ database/migrations/2024_02_22_063554_create_email_queues_table.php
@@ -1,27 +1,39 @@
 <?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
+class CreateEmailQueuesTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('email_queues', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('linkable_id');
+            $table->string('linkable_type', 50);
+            $table->string('subject', 150);
+            $table->string('email', 100);
+            $table->string('view', 50);
+            $table->json('params');
+            $table->enum('status', ['pending', 'processing', 'completed', 'failed']);
+            $table->timestamps(); // This will create both 'created_at' and 'updated_at' columns
+            $table->dateTime('sent')->nullable(); 
+            $table->unsignedSmallInteger('attempt');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('email_queues');
     }
-};
+}
