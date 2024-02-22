f9eeea87e6f1f56deea44bbb835e51404616d701
FeFerxo08 <fernandopalacios751@gmail.com>
2024-02-22 02:00:24 -0600

    Model, Seeder, Factory y Migrations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,42 +3,36 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use App\Models\EmailQueue;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
-class UserFactory extends Factory
+class EmailQueueFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
+     * The name of the factory's corresponding model.
+     *
+     * @var string
      */
-    protected static ?string $password;
+    protected $model = EmailQueue::class;
 
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'linkable_id' => $this->faker->randomNumber(),
+            'linkable_type' => $this->faker->word,
+            'subject' => $this->faker->sentence,
+            'email' => $this->faker->safeEmail,
+            'view' => $this->faker->word,
+            'params' => json_encode($this->faker->words(3)), // asumiendo que params es un array de palabras
+            'status' => $this->faker->randomElement(['pending', 'processing', 'completed', 'failed']),
+            'created_at' => $this->faker->dateTime,
+            'updated_at' => $this->faker->dateTime,
+            'sent' => $this->faker->optional()->dateTime,
+            'attempt' => $this->faker->numberBetween(0, 5)
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
