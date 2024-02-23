9fd659e78ad086734c91708b26d2289805ab14aa
FeFerxo08 <fernandopalacios751@gmail.com>
2024-02-23 12:18:05 -0600

    Estructura del model

--- app/Models/EmailQueue.php
+++ app/Models/EmailQueue.php
@@ -7,5 +7,49 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmailQueue extends Model
 {
-    use HasFactory;
+    // Definir los atributos que son asignables en masa.
+    protected $fillable = [
+        'linkable_id',
+        'linkable_type',
+        'subject',
+        'email',
+        'view',
+        'params',
+        'status',
+        'sent',
+        'attempt'
+    ];
+
+    //Marca de tiempo
+    public $timestamps = true;
+
+    /**
+     * Método para enviar el correo electrónico.
+     *
+     * @return void
+     */
+    public function sendEmail()
+    {
+        //Estructura del correo
+        $mailData = [
+            'subject' => $this->subject,
+            'view' => $this->view,
+            'params' => json_decode($this->params, true)
+        ];
+
+        try {
+            Mail::to($this->email)->send(new YourCustomMailableClass($mailData));
+
+            $this->update([
+                'status' => 'completed',
+                'sent' => now(),
+                'attempt' => $this->attempt + 1
+            ]);
+        } catch (\Exception $e) {
+            $this->update([
+                'status' => 'failed',
+                'attempt' => $this->attempt + 1
+            ]);
+        }
+    }
 }
